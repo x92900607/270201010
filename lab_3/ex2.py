49349f8659e87658f59f8c91493cd8eeab959422
x92900607 <73098009+x92900607@users.noreply.github.com>
2020-10-26 11:32:21 +0300

    Create ex2.py

---
+++ lab_3/ex2.py
@@ -0,0 +1,10 @@
+num1=int(input('enter 1st number:')
+num2=int(input('enter 2nd number:')
+num3=int(input('enter 3rd number:')
+if num1 > num2 and num1> num3:
+  max_num=num1
+elif num2> num3 and num2> num1:
+    max_num=num2
+else:
+max_num=num3
+
